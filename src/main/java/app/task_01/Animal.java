791b97c480a95d7ed92500488a8dab53f5e43074
OlhaOliinyk <olaolijnik71@gmail.com>
2025-01-29 17:55:05 +0100

    task_01

--- src/main/java/app/task_01/Animal.java
+++ src/main/java/app/task_01/Animal.java
@@ -1,9 +1,14 @@
 package app.task_01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Animal {
 
     public abstract String getName();
 
     public abstract int getAge();
+
+
 
 }
---
+++ src/main/java/app/task_01/Main.java
@@ -0,0 +1,18 @@
+package app.task_01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Animal> listOfAnimals = new ArrayList<>();
+
+        listOfAnimals.add(new Hammer());
+        listOfAnimals.add(new Rabbit());
+        listOfAnimals.add(new Rabbit());
+
+
+    }
+}
