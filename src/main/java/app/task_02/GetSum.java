ca8eac9fa29518d7e5eb8d105422868ccc4c50de
OlhaOliinyk <olaolijnik71@gmail.com>
2025-01-29 14:43:00 +0100

    task_01

---
+++ src/main/java/app/task_02/GetSum.java
@@ -0,0 +1,11 @@
+package app.task_02;
+
+public class GetSum {
+
+    private int a;
+    private int b;
+
+    public static int getSum(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/main/java/app/task_02/Main.java
@@ -0,0 +1,10 @@
+package app.task_02;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int result = GetSum.getSum(5, 6);
+
+    }
+}
