cc4ae9f3ba502ae52f0b93afa363c0a30d8154ed
OlhaOliinyk <olaolijnik71@gmail.com>
2025-01-29 14:32:52 +0100

    task_01

---
+++ src/main/java/app/task_01/Animal.java
@@ -0,0 +1,9 @@
+package app.task_01;
+
+public abstract class Animal {
+
+    public abstract String getName();
+
+    public abstract int getAge();
+
+}
---
+++ src/main/java/app/task_01/Hammer.java
@@ -0,0 +1,13 @@
+package app.task_01;
+
+public class Hammer extends Animal {
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public int getAge() {
+        return 0;
+    }
+}
